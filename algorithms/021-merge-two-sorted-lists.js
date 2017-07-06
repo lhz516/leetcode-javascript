146847684734245eed4db4b612465d32babfbe0e
Hanz Luo <lhz0516@hotmail.com>
2017-07-05 23:21:38 -0700

    use recursive call for Q21; optimize original solution of Q21

--- algorithms/021-merge-two-sorted-lists.js
+++ algorithms/021-merge-two-sorted-lists.js
@@ -26,14 +26,28 @@ var mergeTwoLists = function(l1, l2) {
   let headNew = head;
   while(l1 && l2) {
     if (l1.val > l2.val) {
-      headNew.next = new ListNode(l2.val);
+      headNew.next = l2;
       l2 = l2.next;
     } else {
-      headNew.next = new ListNode(l1.val);
+      headNew.next = l1;
       l1 = l1.next;
     }
     headNew = headNew.next;
   }
   headNew.next = l1 ? l1 : l2;
   return head.next;
+};
+
+// Use recursive call
+var mergeTwoListsV2 = function(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoListsV2(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoListsV2(l2.next, l1);
+    return l2;
+  }
 };
